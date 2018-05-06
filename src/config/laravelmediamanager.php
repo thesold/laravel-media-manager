1715ed85d714c54c34ee9403ab824c26c5c92db2
Marco Mark <marco@silvanite.com>
2018-05-06 12:41:40 +0100

    New environment variables to provide better customisation

--- src/config/laravelmediamanager.php
+++ src/config/laravelmediamanager.php
@@ -5,4 +5,6 @@ return [
     "cloud_name" => env('LARAVELMEDIAMANAGER_CLOUD_NAME'),
     "api_key" => env('LARAVELMEDIAMANAGER_API_KEY'),
     "api_secret" => env('LARAVELMEDIAMANAGER_API_SECRET'),
+    "default_folder" => env('LARAVELMEDIAMANAGER_FOLDER', 'media'),
+    "upload_folder" => env('LARAVELMEDIAMANAGER_UPLOAD_FOLDER', 'public/mediamanager'),
 ];
