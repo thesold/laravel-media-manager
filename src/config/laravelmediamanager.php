aea7fa2cd143442b509093ebb3df18f8a046819e
Marco Mark <marco@silvanite.com>
2018-05-07 14:37:06 +0100

    Update default configuration

--- src/config/laravelmediamanager.php
+++ src/config/laravelmediamanager.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    "driver" => env("LARAVELMEDIAMANAGER_DRIVER", "cloudinary"),
+    "driver" => env("LARAVELMEDIAMANAGER_DRIVER", "default"),
     "cloud_name" => env('LARAVELMEDIAMANAGER_CLOUD_NAME'),
     "api_key" => env('LARAVELMEDIAMANAGER_API_KEY'),
     "api_secret" => env('LARAVELMEDIAMANAGER_API_SECRET'),
     "default_folder" => env('LARAVELMEDIAMANAGER_FOLDER', 'media'),
-    "upload_folder" => env('LARAVELMEDIAMANAGER_UPLOAD_FOLDER', 'public/mediamanager'),
+    "upload_folder" => env('LARAVELMEDIAMANAGER_UPLOAD_FOLDER', 'mediamanager'),
 ];
